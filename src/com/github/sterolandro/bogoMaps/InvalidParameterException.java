b7eaeaaa4b936466d69e5a71a471e1403fc3b08e
Sterol Andro <sterol.andro@gmail.com>
2011-02-24 23:54:46 +0100

    TYP Patcher
    TYP Patcher for bogoMapsSigned-off-by: Sterol Andro
    <sterol.andro@gmail.com>

---
+++ src/com/github/sterolandro/bogoMaps/InvalidParameterException.java
@@ -0,0 +1,16 @@
+package com.github.sterolandro.bogoMaps;
+
+public class InvalidParameterException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1768128080888909474L;
+
+	InvalidParameterException() {
+
+	}
+
+	InvalidParameterException(String msg) {
+		super(msg);
+	}
+}
